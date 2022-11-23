3d4cff277f53aab4c929356073295f5b52fb7ad6
s_omidvar <somidvar79@gmail.com>
2022-11-23 15:15:26 +0330

    Made some navigation

--- app/src/main/java/com/example/mynote/MenuFragment.kt
+++ app/src/main/java/com/example/mynote/MenuFragment.kt
@@ -3,9 +3,9 @@ package com.example.mynote
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MenuFragment : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.fragment_menu)
     }
 }
