bc4a17b141a56af87d36d3eb1ff70ec0499cae63
s_omidvar <somidvar79@gmail.com>
2022-11-26 14:36:37 +0330

    edit class EditNote

--- app/src/main/java/com/example/mynote/EditNoteFragment.kt
+++ app/src/main/java/com/example/mynote/EditNoteFragment.kt
@@ -3,7 +3,7 @@ package com.example.mynote
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class EditNoteFragment: AppCompatActivity() {
+class EditNoteFragment : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_edit_note)
