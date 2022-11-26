615ef478410d3e8b853601a7c23db30fa0d1daef
s_omidvar <somidvar79@gmail.com>
2022-11-26 14:04:27 +0330

    Made main kotlin class

---
+++ app/src/main/java/com/example/mynote/EditNoteFragment.kt
@@ -0,0 +1,11 @@
+package com.example.mynote
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditNoteFragment: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_edit_note)
+    }
+}
