fcf848b5607c8fb41e4ac9420e0335399f4a9a79
s_omidvar <somidvar79@gmail.com>
2022-11-27 17:06:19 +0330

    some basic change

---
+++ app/src/main/java/com/example/mynote/SuccessfulDialogFragment.kt
@@ -0,0 +1,11 @@
+package com.example.mynote
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SuccessfulDialogFragment: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.dialog_fragment_successful)
+    }
+}
