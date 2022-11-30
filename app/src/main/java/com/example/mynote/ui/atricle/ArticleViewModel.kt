5b554d25ea0b456170661728ec0cdf07f5c059ef
s_omidvar <somidvar79@gmail.com>
2022-11-30 14:44:18 +0330

    init commit

---
+++ app/src/main/java/com/example/mynote/ui/atricle/ArticleViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.mynote.ui.article
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ArticleViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is slideshow Fragment"
+    }
+    val text: LiveData<String> = _text
+}
